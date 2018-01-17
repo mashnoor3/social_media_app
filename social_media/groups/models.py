5ed8e6f97c27ac38338a6bdc150e93f9095a68e7
Mashnoor Rashid <mashnoorrashid@gmail.com>
2018-01-16 23:00:24 -0500

    created models for groups and posts

--- social_media/groups/models.py
+++ social_media/groups/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from django import template
+
+# allows link embedding, markdowns (like reddit commenting)
+import misaka
+
+User = get_user_model()
+# allows to use custom template tags
+register = template.library()
+
+
+class Group(models.Model):
+    name = models.CharField(mex_length=255,unique=True)
+    slug = models.SlugField(allow_unicode=True,unique=True)
+    description = models.TextField(blank=True,default='')
+    description_html = models.TextField(editable=False,default='',blank=True)
+    members = models.ManyToManyField(User,through='GroupMember')
+
+    def __str__(self):
+        return self.name
+
+    def save(self,*args,**kwargs):
+        self.slug = slugify(self.name)
+        self.description = misaka.html(self.description)
+        super().save(*args,**kwargs)
+
+    def get_absolute_url(self):
+        return reverse('groups:single',kwargs={'slug':self.slug})
+
+    class Meta:
+        ordering = ['name']
+
+class GroupMember(models.Model):
+    # GroupMember is related to Group with this foreign key, which is called 'memberships'
+    group = models.ForeignKey(Group, related_name='memberships')
+    user = models.ForeignKey(User, related_name='user_groups')
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        unique_together = ('group','user')
--- social_media/posts/models.py
+++ social_media/posts/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+from django.conf import settings
 
-# Create your models here.
+import misaka
+
+from groups.models import Group
+
+from django.contrib.auth import get_user_model
+# get the current user logged into the session
+User = get_user_model()
+
+class Post(models.Model):
+    # to do: set up attribute, save method, string method, get absolute url method (once someone has posted something, where to send them), and Meta
+    user = models.ForeignKey(User,related_name='posts')
+    created_at = models.DateTimeField(auto_now=True)
+    message = models.TextField()
+    message_html = models.TextField(editable=False)
+    group = models.ForeignKey(group,related_name='posts',null=True,blank=True)
+
+    def __str__(self):
+        return self.message
+
+    def save(self,*args,**kwargs):
+        # So if ppl has markdowns (ie links) in their post doesn't include all the HTML tags
+        self.message_html=misaka.html(self.message)
+        super().save(*args,**kwargs)
+
+    def get_absolute_url(self):
+        # two params: URL and the keyword dict
+        return reverse('posts:single',kwargs={'username':self.user.username,
+                                                'pk':self.pk})
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['user','message']
