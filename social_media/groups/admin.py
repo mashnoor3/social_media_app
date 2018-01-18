971c7105700587f4cb5d67878b49675f11e98f02
Mashnoor Rashid <mashnoorrashid@gmail.com>
2018-01-17 23:48:01 -0500

    for groups, added views, registered models, and created views

--- social_media/groups/admin.py
+++ social_media/groups/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+# Using inline, allows to edit model on the same page as the parent model
+# GroupMember's parent model is Group
+class GroupMemberInline(admin.TabularInline):
+    model = models.GroupMember
+
+admin.site.register(models.Group)
--- social_media/groups/views.py
+++ social_media/groups/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.auth.mixins.auth.mixins import (LoginRequiredMixin,
+                                            PermissionRequiredMixin)
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+from groups.models import Group,GroupMember
+
+class CreateGroup(LoginRequiredMixin,generic.CreateView):
+    # Users should only be able to modify these two attributes of a group
+    fields = ('name','description')
+    # Connect to the group model
+    model = Group
+
+# Details of the single view
+class SingleGroup (generic.DetailView):
+    model = Group
+
+class ListGroups(generic.ListView):
+    model = Group
